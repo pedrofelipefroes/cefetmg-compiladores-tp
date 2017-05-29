6e971c095e9e3f4c74de8b7e41a1e5417dc94711
Pedro Felipe Froes <ac-pfroes@ac-pfroes-MAC.local>
2017-05-29 12:42:24 -0300

    Adding tags CONST_ZERO, CONST_NOT_ZERO, CONST_ASCII, CONST_UNDERSCORE on Tag.java

--- src/symbol/Tag.java
+++ src/symbol/Tag.java
@@ -1,23 +1,24 @@
 package symbol;
 
 public class Tag {
-	public final static int
-		// auxiliary symbols
-		ASSIGN = 256, COM = 257, DOT_COM = 258, PAR_OPEN = 259, PAR_CLOSE = 260, QUOTE = 65533,
+    public final static int
+            // auxiliary symbols
+            ASSIGN = 256, COM = 257, DOT_COM = 258, PAR_OPEN = 259, PAR_CLOSE = 260, QUOTE = 65533,
 
-		// end of file
-		EOF = 65535,
-		
-		// identifier
-		ID = 270,
+    // end of file
+    EOF = 65535,
 
-		// numeric constant, literal and operators
-		CONST = 271, DIVIDE = 272, LITERAL = 273, MULTIPLY = 274, SUBTRACT = 275, SUM = 276,
+    // identifier
+    ID = 270,
 
-		// relational operators
-		EQUAL = 285, GREATER = 286, GREATER_EQUAL = 287, LOWER = 288, LOWER_EQUAL = 289, NOT_EQUAL = 290,
+    // numeric constant, literal and operators
+    CONST = 271, DIVIDE = 272, LITERAL = 273, MULTIPLY = 274, SUBTRACT = 275, SUM = 276, CONST_ZERO = 277,
+            CONST_NOT_ZERO = 278, CONST_ASCII = 279, CONST_UNDERSCORE = 280,
 
-		// reserved words
-		AND = 300, BEGIN = 301, DO = 302, ELSE = 303, END = 304, IF = 305, INIT = 306, INTEGER = 307, IS = 308, NOT = 309,
-		OR = 310, READ = 311, STOP = 312, STRING = 313, WHILE = 314, WRITE = 315;
+    // relational operators
+    EQUAL = 285, GREATER = 286, GREATER_EQUAL = 287, LOWER = 288, LOWER_EQUAL = 289, NOT_EQUAL = 290,
+
+    // reserved words
+    AND = 300, BEGIN = 301, DO = 302, ELSE = 303, END = 304, IF = 305, INIT = 306, INTEGER = 307, IS = 308, NOT = 309,
+            OR = 310, READ = 311, STOP = 312, STRING = 313, WHILE = 314, WRITE = 315;
 }
